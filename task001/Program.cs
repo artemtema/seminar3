4f3fcde45aeb8ac7ebcd23ab19974750ce49871b
artemtema <temaboychuk@gmail.com>
2023-01-13 21:23:42 +0530

    Добавил задачу 1

---
+++ task001/Program.cs
@@ -0,0 +1,25 @@
+﻿//Напишите программу, которая принимает на вход пятизначное число 
+//и проверяет, является ли оно палиндромом.
+
+//14212 -> нет
+
+//12821 -> да
+
+//23432 -> да
+
+
+Console.WriteLine("Введите число : ");
+int nums = Convert.ToInt32(Console.ReadLine());
+
+string checkPolindrome (int num)
+{
+    int pal = 0; 
+    int number = num;
+    while (number > 0) 
+    {
+        pal = pal * 10 + number % 10;
+        number = number / 10;
+    }
+    return num == pal ? "палиндром" : "не палиндром";
+}
+Console.WriteLine(checkPolindrome(nums));
